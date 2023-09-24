272c2e895b0c3be79f32dd1eba7bf223f669fea7
Aidyn Short <109443741+Arshort@users.noreply.github.com>
2023-09-24 16:59:16 -0400

    finished rng

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,27 @@
-// add your imports here
+#include "iostream"
+using  namespace  std;
+
 int main(){
-  // code here
+unsigned int S,N, value;
+signed int  min,max;
+cin >> S;
+cin >> N;
+cin >> min;
+cin >> max;
+
+if (min > max)
+{
+  int temp = min;
+  min = max;
+  max = temp;
+}
+
+for (int i = 0; i < N; ++i) {
+  S = S xor (S << 13);
+  S = S xor (S >> 17);
+  S =S xor (S << 5);
+  value = min + (S % (max - min +1));
+  cout << value << endl;
+}
+
 }
